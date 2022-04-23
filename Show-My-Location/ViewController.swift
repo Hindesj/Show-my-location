facdd97abfa7d601a108791ee5850bb2727f0a3c
hindes33 <101994279+Hindesj@users.noreply.github.com>
2022-04-22 23:42:14 -0400

    app

--- Show-My-Location/ViewController.swift
+++ Show-My-Location/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate {
+   
+    var latitude: Double?
+    var longitude: Double?
+    var altitude: Double?
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var myMap: MKMapView!
+    @IBOutlet weak var myLatitude: UITextField!
+    @IBOutlet weak var myLongitude: UITextField!
+    @IBOutlet weak var myAltitude: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
+        if status == .authorizedWhenInUse{
+            print("GPS allowed.")
+            myMap.showsUserLocation = true
+        }
+        else{
+            print("GPS not allowed")
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        let myCoordinate = locationManager.location?.coordinate
+        altitude = locationManager.location?.altitude
+        latitude = myCoordinate?.latitude
+        longitude = myCoordinate?.longitude
+        
+        myLatitude.text = String(latitude!)
+        myLongitude.text = String(longitude!)
+        myAltitude.text = String(altitude!)
     }
 
 
